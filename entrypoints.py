c017b2014601f0faa1b1ea583f33301eb06816bb
Mike Boers <mboers@keypics.com>
2012-12-10 10:14:37 -0800

    Move entrypoints into final location

---
+++ entrypoints.py
@@ -0,0 +1,8 @@
+# For backwards compatibility.
+
+# Establish the package path.
+import metatools.entrypoints
+__path__ = metatools.entrypoints.__path__
+
+# Grab the globals.
+from metatools.entrypoints import *
