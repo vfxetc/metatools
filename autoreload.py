ccb80d5c953b534e8cda6024113f694b5c2983ad
Mike Boers <mboers@keypics.com>
2012-12-10 15:00:26 -0800

    Replace stubs with symlinks

--- autoreload.py
+++
@@ -1,9 +0,0 @@
-# For backwards compatibility.
-
-# Establish the package path.
-import metatools.imports
-__path__ = metatools.imports.__path__
-
-# Grab the globals.
-from metatools.imports import *
-
--- entrypoints.py
+++
@@ -1,9 +0,0 @@
-# For backwards compatibility.
-
-# Establish the package path.
-import metatools.entrypoints
-__path__ = metatools.entrypoints.__path__
-
-# Grab the globals.
-from metatools.entrypoints import *
-
