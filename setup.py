f0ea647cdfef5c7940594479f5b22e0691bc795f
Mike Boers <mboers@keypics.com>
2013-01-15 09:15:19 -0800

    Meta: add generic setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='metatools',
+    version='0.1.0b',
+    description='Python tools for Python tools.',
+    url='http://github.com/westernx/metatools',
+    
+    packages=['metatools'],
+    
+    author='Mike Boers',
+    author_email='metatools@mikeboers.com',
+    license='BSD-3',
+    
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    
+)
