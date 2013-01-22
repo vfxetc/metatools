67979c995009fba202ae930cab1c83e677bb59ed
Mike Boers <mboers@keypics.com>
2013-01-22 10:27:33 -0800

    bc: renamed_attr

--- tests/test_back_compat.py
+++ tests/test_back_compat.py
@@ -11,7 +11,8 @@ class TestRenamedModule(TestCase):
 
     def setUp(self):
         self.module_sandbox = os.path.splitext(__file__)[0] + '_sandbox'
-        sys.path.insert(0, self.module_sandbox)
+        if self.module_sandbox not in sys.path:
+            sys.path.insert(0, self.module_sandbox)
 
     def tearDown(self):
         if sys.path[0] == self.module_sandbox:
@@ -38,5 +39,65 @@ class TestRenamedModule(TestCase):
         self.assertTrue(issubclass(w[0].category, back_compat.ModuleRenamedWarning))
         self.assertEqual(w[0].message.args[0], 'test_bc_pack.old was renamed to test_bc_pack.new')
         self.assertEqual(test_bc_pack.use.func(1, 2), 3)
+
+
+class TestRenamedAttribute(TestCase):
+
+    def test_instance_value(self):
+
+        class Example(object):
+
+            def __init__(self):
+                self.new = 1
+
+            old = back_compat.renamed_attr('new')
+
+        e = Example()
+
+        with warnings.catch_warnings(record=True) as w:
+            self.assertEqual(1, e.old)
+        self.assertEqual(len(w), 1)
+        self.assertTrue(issubclass(w[0].category, back_compat.AttributeRenamedWarning))
+        self.assertEqual(w[0].message.args[0], 'Example.old was renamed to new')
+
+        with warnings.catch_warnings(record=True) as w:
+            e.old = 2
+            self.assertEqual(2, e.new)
+        self.assertEqual(len(w), 1)
+        self.assertTrue(issubclass(w[0].category, back_compat.AttributeRenamedWarning))
+        self.assertEqual(w[0].message.args[0], 'Example.old was renamed to new')
+
+    def test_class_value(self):
+
+        class Example(object):
+            new = 1
+            old = back_compat.renamed_attr('new')
+
+        e = Example()
+
+        with warnings.catch_warnings(record=True) as w:
+            self.assertEqual(1, Example.old)
+            self.assertEqual(1, e.old)
+        self.assertEqual(len(w), 2)
+        self.assertTrue(issubclass(w[0].category, back_compat.AttributeRenamedWarning))
+        self.assertEqual(w[0].message.args[0], 'Example.old was renamed to new')
+
+        with warnings.catch_warnings(record=True) as w:
+            e.old = 2
+            self.assertEqual(2, e.new)
+            self.assertEqual(1, Example.old)
+            self.assertEqual(1, Example.new)
+        self.assertEqual(len(w), 2)
+        self.assertTrue(issubclass(w[0].category, back_compat.AttributeRenamedWarning))
+        self.assertEqual(w[0].message.args[0], 'Example.old was renamed to new')
+
+
+
+
+
+
+
+
+
 
 
