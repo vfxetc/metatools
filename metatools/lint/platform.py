a17d85d0b55edb960cfa8189d3d11f0175a7785b
Mike Boers <mboers@keypics.com>
2013-02-13 12:47:34 -0800

    metatools.lint.platform
    Converts some uses of platform.system() to
    sys.platform.

---
+++ metatools/lint/platform.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import re
+
+
+direct_re = re.compile(r'''
+
+    platform\.system \s* \(\s*\)
+    \s*==\s*
+    [rRuUbB]?['"]+(Darwin|Linux|Windows)['"]+
+
+''', re.VERBOSE)
+
+
+def direct_replace(m):
+    return 'sys.platform.startswith("%s")' % {
+        "Darwin": "darwin",
+        "Linux": "linux",
+        "Windows": "win32",
+    }[m.group(1)]
+
+
+for arg in sys.argv[1:]:
+    for dir_name, dir_names, file_names in os.walk(arg):
+
+        # Filter dotted paths out.
+        dir_names[:] = [x for x in dir_names if not x.startswith('.')]
+
+        file_names = [x for x in file_names if x.endswith('.py') and not x.startswith('.')]
+        for file_name in file_names:
+
+            path = os.path.join(dir_name, file_name)
+            with open(path) as fh:
+                source = fh.read()
+
+            source, count = direct_re.subn(direct_replace, source)
+            if count:
+
+                print path
+
+                # Make sure it has a "import sys" at the top.
+                m = re.search(r'^import sys', source, re.MULTILINE)
+                if not m:
+
+                    # Put it before the platform import.
+                    m = re.search(r'^import platform', source, re.MULTILINE)
+
+                    if not m:
+                        print '\tNo platform import; cannot auto import sys!'
+                        continue
+
+                    source = source[:m.start(0)] + 'import sys\n' + source[m.start(0):]
+
+                with open(path, 'wb') as fh:
+                    fh.write(source)
+
+
