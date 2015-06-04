a6fc8c555cef2e3f02c1c7ea81a77a9c08dba2d1
Mike Boers <westernx@mikeboers.com>
2015-06-04 12:35:52 -0700

    App bootstrappers defer importing site until after they modify the environment

--- metatools/apps/templates/bootstrap.c
+++ metatools/apps/templates/bootstrap.c
@@ -3,15 +3,16 @@
 
 int main(int argc, char **argv) {
 
+    // Disable importing the site module. We will manually import it in
+    // the bootstrapper, but we want to be able to modify sys.path before
+    // site runs (which imports a few WesternX packages that we may want
+    // to override via sys.path).
+    Py_NoSiteFlag = 1;
+    
     Py_SetProgramName(argv[0]);
     Py_Initialize();
 
-    PyObject *myArgv = PyList_New(argc);
-    for (int i = 0; i < argc; i++) {
-        PyList_SET_ITEM(myArgv, i, PyString_FromString(argv[i]));
-    }
-    PySys_SetObject("argv", myArgv);
-
+    PySys_SetArgvEx(argc, argv, 0);
     PyRun_SimpleString(
         #ifdef METATOOLS_BOOTSTRAP_SOURCE
             METATOOLS_BOOTSTRAP_SOURCE
